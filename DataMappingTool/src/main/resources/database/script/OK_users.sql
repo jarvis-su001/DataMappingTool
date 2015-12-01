03c8a35857dfa7ad40328a946ac0dd26b3ded92c
milo_scorpio <milo_scorpio@126.com>
2015-12-01 11:41:37 +0800

    2222

--- DataMappingTool/src/main/resources/database/script/OK_users.sql
+++ DataMappingTool/src/main/resources/database/script/OK_users.sql
@@ -9,16 +9,14 @@ SELECT DISTINCT th.transaction_type_id, tt.descr, tm.module_id, m.descr
        module              m
  WHERE tu.user_id = u.user_id
    AND u.login = '0000'
-   
+      
    AND th.trx_id = tu.trx_id
    AND th.sequence = tu.sequence
    AND tt.transaction_type_id = th.transaction_type_id
-   
+      
    AND th.trx_id = tm.trx_id
    AND th.sequence = tm.sequence
-   AND tm.module_id = m.module_id
-   
-   ;
+   AND tm.module_id = m.module_id;
    
    SELECT * FROM MODULE;
 /*
@@ -93,6 +91,13 @@ SELECT * FROM provider p WHERE p.provider_id='15369';
  SELECT * FROM USERS u WHERE u.user_id='31';
  
 SELECT MAX(th.entry_datetime), tm.module_id
+  FROM transaction_history th, transaction_module tm
+ WHERE th.trx_id = tm.trx_id
+   AND th.sequence = tm.sequence
+   AND th.transaction_type_id = '4034'
+ GROUP BY tm.module_id;
+ 
+ SELECT MAX(th.entry_datetime), tm.module_id
   FROM transaction_history th, transaction_module tm
  WHERE th.trx_id = tm.trx_id
    AND th.sequence = tm.sequence
