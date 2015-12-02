9604dc49056e90f4076317ac16b844094c8a8b03
milo_scorpio <milo_scorpio@126.com>
2015-12-02 13:38:08 +0800

    add test

---
+++ DataMappingTool/src/main/java/com/jarvis/FileTest.java
@@ -0,0 +1,15 @@
+package com.jarvis;
+
+import com.jarvis.tools.FileTools;
+
+/**
+ * Created by C5023792 on 12/2/2015.
+ */
+public class FileTest {
+
+    public static void main(String[] args) {
+
+        String path = "E:/temp";
+        FileTools.getAllFiles(path);
+    }
+}
